171eb963bcb2e264d63a917c8905100791850008
WiseHacks <iit2019063@iiita.ac.in>
2022-11-16 07:55:16 +0530

    minor change

--- app/src/main/java/com/example/emosic/data/Song.kt
+++ app/src/main/java/com/example/emosic/data/Song.kt
@@ -5,6 +5,6 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 @Parcelize
 /*TODO - Firestore collections*/
-data class Video(
+data class Song(
     var title: String = "",
 ) : Parcelable
